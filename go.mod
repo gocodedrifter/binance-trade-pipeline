48fff13534cafd11ed94cfa70a94e7302c9c1739
arip <=>
2021-02-23 15:08:11 +0700

    init websocket

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gocodedrifter/coinpanel
+
+go 1.12
+
+require github.com/gorilla/websocket v1.4.2
---
+++ pkg/websocket/websocket_service.go
@@ -0,0 +1,59 @@
+package websocket
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
+)
+
+// Endpoints
+const (
+	baseWsMainURL = "wss://stream.binance.com:9443/ws"
+)
+
+var (
+	// WebsocketTimeout is an interval for sending ping/pong messages if WebsocketKeepalive is enabled
+	WebsocketTimeout = time.Second * 60
+	// WebsocketKeepalive enables sending ping/pong messages to check the connection stability
+	WebsocketKeepalive = false
+)
+
+// getWsEndpoint return base ws main url
+func getWsEndpoint() string {
+	return baseWsMainURL
+}
+
+// WsTradeHandler handle websocket trade event
+type WsTradeHandler func(event *WsTradeEvent)
+
+// WsTradeServe serve websocket handler with a symbol
+func WsTradeServe(symbol string, handler WsTradeHandler, errHandler ErrHandler) (doneC, stopC chan struct{}, err error) {
+	endpoint := fmt.Sprintf("%s/%s@trade", getWsEndpoint(), strings.ToLower(symbol))
+	cfg := newWsConfig(endpoint)
+	wsHandler := func(message []byte) {
+		event := new(WsTradeEvent)
+		err := json.Unmarshal(message, event)
+		if err != nil {
+			errHandler(err)
+			return
+		}
+		handler(event)
+	}
+	return wsServe(cfg, wsHandler, errHandler)
+}
+
+// WsTradeEvent define websocket trade event
+type WsTradeEvent struct {
+	Event         string `json:"e"`
+	Time          int64  `json:"E"`
+	Symbol        string `json:"s"`
+	TradeID       int64  `json:"t"`
+	Price         string `json:"p"`
+	Quantity      string `json:"q"`
+	BuyerOrderID  int64  `json:"b"`
+	SellerOrderID int64  `json:"a"`
+	TradeTime     int64  `json:"T"`
+	IsBuyerMaker  bool   `json:"m"`
+	Placeholder   bool   `json:"M"` // add this field to avoid case insensitive unmarshaling
+}
